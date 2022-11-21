19cba6b168af625206ef3578aae488fc43909245
Tais Guissi <71243004+TaisGuissi@users.noreply.github.com>
2022-11-21 14:32:04 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PORTARIA</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/seletor.css" rel="stylesheet">
+    <link href="css/estilo.css" rel="stylesheet">
+
+
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link href="css/seletor.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/inicio.css">
+    <!--===============================================================================================-->
+
+
+
+
+
+
+
+
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+
+        <div class="wrap-login100">
+            <fieldset>
+
+                <span class="login100-form-title p-b-34 p-t-27">
+                    PORTARIA TESSIN
+                </span>
+                <li>
+                    <a class="nav-link" href="funcionarios.php">
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">
+                                FUNCIONÁRIOS
+                            </button>
+                        </div>
+                </li>
+
+                <a class="nav-link" href="veiculos.php">
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn">
+                            VEICÚLOS
+                        </button>
+                    </div>
+            </fieldset>
+        </div>
+   
+
