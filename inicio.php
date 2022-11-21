bb96c9a96292ae3762f794237ddb8a49460b8932
Tais Guissi <71243004+TaisGuissi@users.noreply.github.com>
2022-11-21 17:36:36 -0300

    css

--- inicio.php
+++ inicio.php
@@ -27,6 +27,17 @@
     <link href="css/seletor.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/inicio.css">
 <!--===============================================================================================-->
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
 </head>
 <body>
 	
@@ -42,8 +53,9 @@
 						PORTARIA TESSIN
 					</span>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
+
+					<div class="container-login100-form-btn" href="#myModal">
+						<button class="login100-form-btn" data-toggle="modal" data-target="#myModal">
 							VISITANTES 
 						</button>
 					</div>
@@ -73,7 +85,65 @@
 		</div>
 	</div>
 	
+
+
+
+
+
+
+
+
+	
 	<div id="dropDownSelect1"></div>
+
+
+
+
+
+	<div class="container">
+ 
+ <!-- Button to Open the Modal -->
+ <button type="button" class="login100-form-btn" data-toggle="modal" data-target="#myModal">
+   Open modal
+ </button>
+
+ <!-- The Modal -->
+ <div class="modal" id="myModal">
+   <div class="modal-dialog">
+	 <div class="modal-content">
+	 
+	   <!-- Modal Header -->
+	   <div class="modal-header">
+		 <h4 class="modal-title">Modal Heading</h4>
+		 <button type="button" class="close" data-dismiss="modal">&times;</button>
+	   </div>
+	   
+	   <!-- Modal body -->
+	   <div class="modal-body">
+		 Modal body..
+	   </div>
+	   
+	   <!-- Modal footer -->
+	   <div class="modal-footer">
+		 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+	   </div>
+	   
+	 </div>
+   </div>
+ </div>
+
+	
+
+
+
+
+
+
+
+
+
+
+
 	
 <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
--- teste.php
+++ teste.php
@@ -20,6 +20,17 @@
     <link href="css/seletor.css" rel="stylesheet">
     <link href="css/estilo.css" rel="stylesheet">
 
+      <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/seletor.css" rel="stylesheet">
+    <link href="css/estilo.css" rel="stylesheet">
+
+
 
     <!--===============================================================================================-->
     <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
@@ -43,17 +54,14 @@
     <link rel="stylesheet" type="text/css" href="css/util.css">
     <link href="css/seletor.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/inicio.css">
+    <link rel="stylesheet" type="text/css" href="css/teste.css">
     <!--===============================================================================================-->
-
-
-
-
-
-
-
-
-
-
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body id="page-top">
@@ -62,6 +70,9 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
+         <!-- Sidebar -->
+         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
 
         <div class="wrap-login100">
             <fieldset>
@@ -69,6 +80,96 @@
                 <span class="login100-form-title p-b-34 p-t-27">
                     PORTARIA TESSIN
                 </span>
+        
+
+                    <a class="nav-link" href="#myModal">
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn" data-toggle="modal" data-target="#myModal">
+                                VISITANTES
+                            </button>
+
+
+
+                            
+                        </div>
+                        
+
+
+                        
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Modal Heading</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+        <div class="limiter">
+		<div class="container-login100" style="">
+			<div class="">
+				
+	
+
+                    <div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							ALTERAR 
+						</button>
+					</div>
+
+                    <BR>
+					
+                    <div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							CONSULTAR 
+						</button>
+					</div>
+
+					
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+	<div id="dropDownSelect1"></div>
+
+	<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+                </li>
+
                 <li>
                     <a class="nav-link" href="funcionarios.php">
                         <div class="container-login100-form-btn">
@@ -78,13 +179,50 @@
                         </div>
                 </li>
 
-                <a class="nav-link" href="veiculos.php">
-                    <div class="container-login100-form-btn">
-                        <button class="login100-form-btn">
-                            VEICÚLOS
-                        </button>
-                    </div>
+                <li>
+                    <a class="nav-link" href="veiculos.php">
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">
+                                VEICÚLOS
+                            </button>
+                        </div>
+                </li>
+
+                <li>
+                    <a class="nav-link" href="recebimento.php">
+                        <div class="container-login100-form-btn">
+                            <button class="login100-form-btn">
+                                RECEBIMENTO
+                            </button>
+                        </div>
+                </li>
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
             </fieldset>
-        </div>
-   
+
+             <!-- Divider -->
+             <hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
 
