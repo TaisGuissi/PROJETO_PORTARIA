f98d315237d47de85ff8a33bd5cdb6f13c0df9ad
Tais Guissi <71243004+TaisGuissi@users.noreply.github.com>
2022-11-22 07:39:51 -0300

    Modal

--- inicio.php
+++ inicio.php
@@ -1,46 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang=pt-br">
+
 <head>
 	<title>INICIO</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
-    <link href="css/seletor.css" rel="stylesheet">
+	<link href="css/seletor.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/inicio.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-
-
-
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
 
 </head>
+
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 			<div class="wrap-login100">
@@ -48,119 +47,65 @@
 					<span class="login100-form-logo">
 						<i class="zmdi zmdi-landscape"></i>
 					</span>
-
 					<span class="login100-form-title p-b-34 p-t-27">
 						PORTARIA TESSIN
 					</span>
 
+					<br>
+						<div class="container-login100-form-btn">
+							<button class="login100-form-btn">
+								VISITANTES
+							</button>
+						</div>
+						<br>
 
-					<div class="container-login100-form-btn" href="#myModal">
-						<button class="login100-form-btn" data-toggle="modal" data-target="#myModal">
-							VISITANTES 
-						</button>
-					</div>
-                    <br>
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							FUNCIONÁRIOS 
-						</button>
-					</div>
-                    <br> 
+						<br>
+						<div class="container-login100-form-btn">
+							<button class="login100-form-btn">
+								FUNCIONÁRIOS
+							</button>
+						</div>
+						<br>
 
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							VEICÚLOS 
-						</button>
-					</div>
-                    <br>
+						<div class="container-login100-form-btn">
+							<button class="login100-form-btn">
+								VEICÚLOS
+							</button>
+						</div>
+						<br>
 
-                    <div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							RECEBIMENTO 
-						</button>
-					</div>
-                    <br>
+						<div class="container-login100-form-btn">
+							<button class="login100-form-btn">
+								RECEBIMENTO
+							</button>
+						</div>
+						<br>
 				</form>
 			</div>
 		</div>
 	</div>
-	
 
 
-
-
-
-
-
-
-	
 	<div id="dropDownSelect1"></div>
 
 
-
-
-
-	<div class="container">
- 
- <!-- Button to Open the Modal -->
- <button type="button" class="login100-form-btn" data-toggle="modal" data-target="#myModal">
-   Open modal
- </button>
-
- <!-- The Modal -->
- <div class="modal" id="myModal">
-   <div class="modal-dialog">
-	 <div class="modal-content">
-	 
-	   <!-- Modal Header -->
-	   <div class="modal-header">
-		 <h4 class="modal-title">Modal Heading</h4>
-		 <button type="button" class="close" data-dismiss="modal">&times;</button>
-	   </div>
-	   
-	   <!-- Modal body -->
-	   <div class="modal-body">
-		 Modal body..
-	   </div>
-	   
-	   <!-- Modal footer -->
-	   <div class="modal-footer">
-		 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-	   </div>
-	   
-	 </div>
-   </div>
- </div>
-
-	
-
-
-
-
-
-
-
-
-
-
-
-	
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 </body>
+
 </html>
